4bd7d1c9c6bf0e064c148ce1c9de2dac46d9f5e7
Jeff-Hardcase <32984988+Jeff-Hardcase@users.noreply.github.com>
2022-11-13 22:41:10 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using SpaceStationAPI.Models;
+
+namespace SpaceStationAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.Configure<Settings>(builder.Configuration.GetSection("Settings"));
+            
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
